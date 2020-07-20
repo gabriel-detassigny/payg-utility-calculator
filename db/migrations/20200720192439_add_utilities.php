95883e32d92bdf0ed7632296e02e5d2735320faf
Gabriel de Tassigny <gabriel@multivac.cc>
2020-07-20 20:29:32 +0100

    Add phinx

---
+++ db/migrations/20200720192439_add_utilities.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddUtilities extends AbstractMigration
+{
+    public function change(): void
+    {
+        $utilities = $this->table('utilities');
+        $utilities->addColumn('name', 'string', ['limit' => 50])
+            ->create();
+    }
+}
---
+++ phinx.php
@@ -0,0 +1,18 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'sqlite',
+            'name' => '%%PHINX_CONFIG_DIR%%/db/db',
+            'suffix' => '.sqlite'
+        ]
+    ],
+    'version_order' => 'creation'
+];
